49d5eb1ba1416abde80be888e65c7ced39a811b4
glenmeredith68 <glen.lee.meredith@gmail.com>
2024-04-21 21:43:56 -0600

    idk

--- 04-diner/js/scripts.js
+++ 04-diner/js/scripts.js
@@ -5,7 +5,6 @@ document.querySelector("#year").textContent = year;
 
 function toggleMenu() {
   document.querySelector("nav").classList.toggle("open");
-  document.querySelector("#hamb").classList.toggle("open");
 }
 
 const ham = document.querySelector("#ham").addEventListener("click", () => {
