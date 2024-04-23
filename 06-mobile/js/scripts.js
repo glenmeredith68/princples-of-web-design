8b54d29abf5cebe2bb57dce89136b94ee160b4f0
glenmeredith68 <glen.lee.meredith@gmail.com>
2024-04-22 21:38:22 -0600

    styling

--- 06-mobile/js/scripts.js
+++ 06-mobile/js/scripts.js
@@ -2,3 +2,13 @@
 const rightNow = new Date();
 const year = rightNow.getFullYear();
 document.querySelector('#year').textContent = year;
+
+
+function toggleMenu() {
+    document.querySelector("nav").classList.toggle("open");
+    document.querySelector("#hamb").classList.toggle("open");
+}
+  
+  const ham = document.querySelector("#hamb").addEventListener("click", () => {
+    toggleMenu();
+  });
