f77ed38bb288af0d0ed2ffb251e1faa4048e1e8d
glenmeredith68 <glen.lee.meredith@gmail.com>
2024-04-24 17:32:24 -0600

    Menu styling

--- 09-learning/js/scripts.js
+++ 09-learning/js/scripts.js
@@ -2,3 +2,11 @@
 const rightNow = new Date();
 const year = rightNow.getFullYear();
 document.querySelector('#year').textContent = year;
+
+const ham = document.querySelector('#ham');
+const nav = document.querySelector('#primaryNav');
+
+ham.addEventListener('click', () => {
+    ham.classList.toggle('open');
+    nav.classList.toggle('open');
+})
