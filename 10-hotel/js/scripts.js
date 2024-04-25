08f44e370d0fcb25de8d9abd6b1e5ae62946ad3d
glenmeredith68 <glen.lee.meredith@gmail.com>
2024-04-25 15:41:11 -0600

    added images and json

--- 10-hotel/js/scripts.js
+++ 10-hotel/js/scripts.js
@@ -1,4 +1,15 @@
+import {hotels} from 'hotels.js';
+
 // inject current year to footer
 const rightNow = new Date();
 const year = rightNow.getFullYear();
 document.querySelector('#year').textContent = year;
+
+const ham = document.querySelector('.ham');
+const pw = document.querySelector('.pageWrapper');
+
+ham.addEventListener('click', () => {
+    pw.classList.toggle('moveOver');
+});
+
+const 
