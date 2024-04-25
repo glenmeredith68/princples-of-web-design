443ff5645dfd31440c0e150d91f9bf04894c936b
glenmeredith68 <glen.lee.meredith@gmail.com>
2024-04-25 13:20:15 -0600

    masking

--- 09-learning/js/scripts.js
+++ 09-learning/js/scripts.js
@@ -1,12 +1,28 @@
 // inject current year to footer
 const rightNow = new Date();
 const year = rightNow.getFullYear();
-document.querySelector('#year').textContent = year;
+document.querySelector("#year").textContent = year;
 
-const ham = document.querySelector('#ham');
-const nav = document.querySelector('#primaryNav');
+const ham = document.querySelector("#ham");
+const nav = document.querySelector("#primaryNav");
 
-ham.addEventListener('click', () => {
-    ham.classList.toggle('open');
-    nav.classList.toggle('open');
-})
+ham.addEventListener("click", () => {
+  ham.classList.toggle("open");
+  nav.classList.toggle("open");
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  effect: "cube",
+  speed: 2000,
+  //autoplay
+  autoplay: {
+    delay: 5000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
