ca8423728710983c934884683cdfc58ee2a8754d
glenmeredith68 <glen.lee.meredith@gmail.com>
2024-04-25 16:22:02 -0600

    cards generated script done

--- 10-hotel/js/hotels.js
+++ 10-hotel/js/hotels.js
@@ -1,51 +1,50 @@
 export const hotels = [
-    {
-      "name": "Glenview Inn",
-      "photo": "bed1.jpg",
-      "address": "456 Ocean View Avenue Seaside, CA 93955",
-      "phone": "831-555-1234"
-    },
-    {
-      "name": "Glen Mountain Lodge Resort",
-      "photo": "bed2.jpg",
-      "address": "789 Pine Trail Mountainville, CO 80439",
-      "phone": "303-555-5678"
-    },
-    {
-      "name": "Glen Lakefront Retreat",
-      "photo": "bed3.jpg",
-      "address": "101 Lakeview Drive Lakeside, WA 98011",
-      "phone": "425-555-9012"
-    },
-    {
-      "name": "Glen Urban Oasis Hotel",
-      "photo": "bed4.jpg",
-      "address": "321 City Center Boulevard Metroville, NY 10001",
-      "phone": "212-555-3456"
-    },
-    {
-      "name": "Glen Desert Mirage Resort",
-      "photo": "bed5.jpg",
-      "address": "543 Sand Dune Road Oasis City, AZ 85001",
-      "phone": "602-555-7890"
-    },
-    {
-      "name": "Glen Riverside Inn",
-      "photo": "bed6.jpg",
-      "address": "876 Riverwalk Avenue River City, TX 75001",
-      "phone": "214-555-2345"
-    },
-    {
-      "name": "Glen Harborview Hotel",
-      "photo": "bed7.jpg",
-      "address": "222 Pier Street Harbor Town, CA 90505",
-      "phone": "310-555-6789"
-    },
-    {
-      "name": "Glen Green Valley Resort",
-      "photo": "bed8.jpg",
-      "address": "654 Meadow Lane Paradise Valley, NV 89501",
-      "phone": "775-555-0123"
-    }
-  ]
-  
+  {
+    "name": "Glenview Inn",
+    "photo": "bed1.jpg",
+    "address": ["456 Ocean View Avenue", "Seaside, CA 93955"],
+    "phone": "831-555-1234"
+  },
+  {
+    "name": "Glen Mountain Lodge Resort",
+    "photo": "bed2.jpg",
+    "address": ["789 Pine Trail", "Mountainville, CO 80439"],
+    "phone": "303-555-5678"
+  },
+  {
+    "name": "Glen Lakefront Retreat",
+    "photo": "bed3.jpg",
+    "address": ["101 Lakeview Drive", "Lakeside, WA 98011"],
+    "phone": "425-555-9012"
+  },
+  {
+    "name": "Glen Urban Oasis Hotel",
+    "photo": "bed4.jpg",
+    "address": ["321 City Center Boulevard", "Metroville, NY 10001"],
+    "phone": "212-555-3456"
+  },
+  {
+    "name": "Glen Desert Mirage Resort",
+    "photo": "bed5.jpg",
+    "address": ["543 Sand Dune Road", "Oasis City, AZ 85001"],
+    "phone": "602-555-7890"
+  },
+  {
+    "name": "Glen Riverside Inn",
+    "photo": "bed6.jpg",
+    "address": ["876 Riverwalk Avenue", "River City, TX 75001"],
+    "phone": "214-555-2345"
+  },
+  {
+    "name": "Glen Harborview Hotel",
+    "photo": "bed7.jpg",
+    "address": ["222 Pier Street", "Harbor Town, CA 90505"],
+    "phone": "310-555-6789"
+  },
+  {
+    "name": "Glen Green Valley Resort",
+    "photo": "bed8.jpg",
+    "address": ["654 Meadow Lane", "Paradise Valley, NV 89501"],
+    "phone": "775-555-0123"
+  }
+]
--- 10-hotel/js/scripts.js
+++ 10-hotel/js/scripts.js
@@ -1,4 +1,4 @@
-import {hotels} from 'hotels.js';
+import {hotels} from './hotels.js';
 
 // inject current year to footer
 const rightNow = new Date();
@@ -12,4 +12,29 @@ ham.addEventListener('click', () => {
     pw.classList.toggle('moveOver');
 });
 
-const 
+const cards = document.querySelector('.cards');
+hotels.forEach(hotel => {
+    const sec = document.createElement('section');
+
+    const img = document.createElement('img');
+    img.src = `images/${hotel.photo}`;
+    img.alt = hotel.name;
+    
+    const h2 = document.createElement('h2');
+    h2.textContent = hotel.name;
+
+    const address = document.createElement('address');
+    address.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
+
+    const phone = document.createElement('a');
+    phone.href = `tel:${hotel.phone}`;
+    phone.textContent = hotel.phone;
+
+    sec.appendChild(img);
+    sec.appendChild(h2);
+    sec.appendChild(address);
+    sec.appendChild(phone);
+
+    cards.appendChild(sec);
+
+});
